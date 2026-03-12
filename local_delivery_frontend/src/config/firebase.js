7d697ac3501c033028e029b49a1dc2ab8503b445
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2026-03-12 12:21:44 +0530

    Phone number verififcation done

---
+++ local_delivery_frontend/src/config/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDuqGsuv9Ifp_ShT1m2mNkmAGMaxOaDkq4",
+  authDomain: "barhalganjfooddelivery.firebaseapp.com",
+  projectId: "barhalganjfooddelivery",
+  storageBucket: "barhalganjfooddelivery.firebasestorage.app",
+  messagingSenderId: "779290499097",
+  appId: "1:779290499097:web:7c518c3ecc3e303017f288",
+  measurementId: "G-2KN2Q7RKS7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
