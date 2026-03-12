8bfcf865cdaf501fb4f22203b7a61a6ba35fcfdc
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2026-03-12 12:25:39 +0530

    Phone number verififcation done

--- local_delivery_frontend/src/config/firebase.js
+++ local_delivery_frontend/src/config/firebase.js
@@ -1,6 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+ 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +16,7 @@ const firebaseConfig = {
   measurementId: "G-2KN2Q7RKS7"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);  // ← this is the only line PhoneVerification.jsx needs
+export default app;
+ 
